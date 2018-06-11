67cae0bc0d093254389584cfac1da2b89e75c428
Tobias Herber <40146720150275@litec.ac.at>
2018-06-11 13:55:00 +0200

    add cli

---
+++ cli.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+const mri = require('mri');
+const server = require('./src/server');
+
+const args = process.argv.slice(2);
+
+const parsed = mri(args, {
+  alias: {
+    port: 'port'
+  },
+  default: {
+    port: 3000
+  }
+});
+
+server(parsed.port);
+
+console.log('🎉 Listening to port: ' + parsed.port);
