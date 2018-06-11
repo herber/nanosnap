31ed2df423e2337c463fc1251775ed77bd41fe66
Tobias Herber <40146720150275@litec.ac.at>
2018-06-11 14:00:26 +0200

    add server

---
+++ src/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const render = require('./render');
+const cache = require('memory-cache');
+
+module.exports = port => {
+  const app = express();
+
+  app.use(cors());
+
+  app.get('/', async (req, res) => {
+    res.type('.png');
+
+    const c = cache.get(req.query.url);
+
+    if (c !== null) {
+      res.send(c);
+    } else {
+      const screenshot = await render(req.query.url || 'https://notfound.tobihrbr.gq', req.query);
+
+      res.send(screenshot);
+      cache.put(req.query.url, screenshot, 50000);
+    }
+  });
+
+  app.listen(port);
+
+  return app;
+};
